846968ba8b8074ce534419b450fcd6336c100c75
Afsal <afsalshajibismi@gmail.com>
2025-02-01 16:11:20 +0530

    Added Explanation Comments

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ import serial.tools.list_ports
 import serial
 import time
 
+"""The Sotware is a personal project I did to learn more about Arduino. But eventually I learnt
+the CTKinter module also. It was a great learning experience and I will be adding more features
+as time goes by"""
+
 
 class LEDstrip():
 
     def __init__(self, root):
         self.root = root
-        self.xpos = 0.71
-        self.running_rainbow = False
-        self.is_locked = False
-        self.selected_color = None
-        self.is_on = True
+        self.xpos = 0.71 #allingment of Labels, Slider and Buttons in X Axis
+        self.running_rainbow = False #flag for rainbow mode
+        self.is_locked = False #flag for color selection
+        self.selected_color = None # selected color RGB values in tuple
+        self.is_on = True #flag for ON/OFF
 
         self.arduino = Arduino() 
         self.root.title("Color Selector")
@@ -40,15 +44,17 @@ class LEDstrip():
 
         self.color_label = tk.CTkLabel(root, text="0 0 0",font=("Arial", 40, "bold"),text_color = "#fffcf2" )
         self.color_label.place(relx=self.xpos, rely=0.22, anchor="center")
-        
-        self.selected_label = tk.CTkLabel(
+        #The above code displays the color wheel
+
+        self.selected_text = tk.CTkLabel(
             self.root,
             text="Selected Color",
             font=("Arial", 12),
             text_color="#fffcf2",
             anchor="center")
         self.selected_label.place(relx=self.xpos, rely=0.26, anchor="center")
-
+        #For the "Selected Color Text"
+        
         self.brightness_slider = tk.CTkSlider(self.root, 
             from_=10,
             to=100,
@@ -63,7 +69,6 @@ class LEDstrip():
             command=self.sliderchange)
         self.brightness_slider.set(100)
         self.brightness_slider.place(relx=self.xpos, rely=0.39, anchor="center")
-
         self.selected_label = tk.CTkLabel(
             self.root,
             text="Brightness",
@@ -71,6 +76,7 @@ class LEDstrip():
             text_color="#ffffff",
             anchor="center")
         self.selected_label.place(relx=self.xpos, rely=0.43, anchor="center")
+        #Brightness Slider and Text
 
         self.switchR = tk.CTkSwitch(self.root,
             text="Rainbow Mode",
@@ -81,6 +87,7 @@ class LEDstrip():
             button_color="#fffcf2",
             command=self.rainbowOn)
         self.switchR.place(relx=self.xpos, rely=0.57, anchor="center")
+        #Toggle Button for Rainbow Mode
 
         self.onoff = tk.CTkButton(
             self.root,
@@ -92,6 +99,7 @@ class LEDstrip():
             font=("Arial", 20, "bold"),
             corner_radius=60)
         self.onoff.place(relx=self.xpos, rely=0.75, anchor="center")
+        #ON/OFF Button
 
         anothercolor = tk.CTkButton(self.root,
             text="Choose Another Color",
@@ -105,10 +113,11 @@ class LEDstrip():
             corner_radius=60,
             fg_color="#252422")
         anothercolor.place(relx=self.xpos, rely=0.83, anchor="center")
+        #Button to enable user to select another color once a color is selected already
 
         self.color_wheel_label.bind("<Motion>", self.on_motion)
         self.color_wheel_label.bind("<Button-1>", self.on_click)
-
+        #For tracking mouse movements and show the color in real time
 
     def resource_path(self, relative_path):
         try:
@@ -116,7 +125,7 @@ class LEDstrip():
         except Exception:
             base_path = os.path.abspath(".")
         return os.path.join(base_path, relative_path)
-
+    #To get absolute path. Using this to avoid error while using PyInstaller
 
     def create_color_wheel(self, size=380):
         image = Image.new('RGB', (size, size), color="#252422")
@@ -126,6 +135,8 @@ class LEDstrip():
             color = ImageColor.getrgb(f"hsl({angle}, 100%, 50%)")
             draw.pieslice([0, 0, size, size], start=angle, end=angle + 1, fill=color)
         return image
+    """This function creates a color wheel using Pillow module. Each angle is taken as hue and a pie
+    slice is created. This pie slice is filled with cooresponding hue""" 
 
     def get_color_from_position(self, x, y, size=380):
         center = (size // 2, size // 2)
@@ -134,6 +145,8 @@ class LEDstrip():
         angle = math.degrees(math.atan2(dy, dx)) % 360
         color = ImageColor.getrgb(f"hsl({angle}, 100%, 50%)")
         return color
+    """This function fetches mouses position and calculates the distance from the centre. Using that 
+    data, the angle is calculated, hence hue. It returns the color color data"""
 
     def adjust_brightness(self, color, brightness):
         r, g, b = color
@@ -142,6 +155,8 @@ class LEDstrip():
         g = int(g * brightness_factor)
         b = int(b * brightness_factor)
         return (r, g, b)
+    """Control the brighness of the light by returning the the selected colors 1/(10 to 100) th 
+    value. Returns a tuple"""
 
     def on_motion(self, event):
         if self.is_locked or not self.is_on or self.running_rainbow:  
@@ -155,6 +170,8 @@ class LEDstrip():
 
         self.color_label.configure(text=color, font=("Arial", 40, "bold"), text_color="#fffcf2")
         self.color_label.place(relx=self.xpos, rely=0.22, anchor="center")
+    """Checks the mouse data and if the mouse is hovering above the color wheel, it returns the
+    corresponding colors"""
 
     def on_click(self, event): 
         if self.is_locked: 
@@ -169,12 +186,14 @@ class LEDstrip():
                 bright = self.brightness_slider.get()
                 f = self.adjust_brightness(self.selected_color, bright)
                 self.arduino.send_rgb_to_arduino(f)
+    """Selects the color when left clicks on the color wheel"""
 
     def sliderchange(self, brightness):
         if self.is_on:
             if self.selected_color:
                 f = self.adjust_brightness(self.selected_color, brightness)
                 self.arduino.send_rgb_to_arduino(f)
+    """This function adjusts the brightness and send to the Arduino"""
 
     def toggle_button(self):
         if self.is_on:
@@ -185,6 +204,7 @@ class LEDstrip():
             self.onoff.configure(text="On", fg_color="#5CB85C")
             self.turnon()
             self.is_on = True
+        """Toggles on and of Function"""
 
     def changeColor(self):
         global is_locked
@@ -211,6 +231,8 @@ class LEDstrip():
             self.arduino.send_rgb_to_arduino(rgb)
             hue = (hue + 1) % 360 
             time.sleep(0.05)
+        """This works by constalntly looping the hue from o-360 until the running_rainbow
+        yeilds Flase"""
 
     def rainbowOn(self):
         if self.switchR.get():  
@@ -255,20 +277,25 @@ class LEDstrip():
 
 class Arduino():
     def __init__(self):
-        # Port = self.checkPort()
+        #Connects to the Arduino
+        Port = self.checkPort()
         self.arduino = serial.Serial(self.checkPort(), 9600)
 
     def send_rgb_to_arduino(self, rgb):
+        """This Function send the collected RGB values to arduino in 
+        Binary Format"""
         try:
             if self.arduino.is_open:
                 r, g, b = rgb
                 self.arduino.write(bytes([r, g, b])) 
-                time.sleep(0.01)  
+                time. sleep(0.01)  
 
         except serial.SerialException:
             print("ERROR")
 
     def checkPort(self):
+        """Automatically checks in which port the Arduino is connected
+        Error handling to be added"""
         ports = serial.tools.list_ports.comports()
         for port in ports:
             if "Arduino" in port.description:
@@ -276,7 +303,7 @@ class Arduino():
         return False
 
 
-
+#Main Function
 if __name__ == "__main__":
     root = tk.CTk(fg_color="#252422")
     main = LEDstrip(root)
